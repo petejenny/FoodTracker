2893dfdee24ebb6f89798b022c856df59b17c2d3
Peter Forward <pgforward@gmail.com>
2019-01-05 17:00:21 -0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Peter Forward on 1/5/19.
+//  Copyright © 2019 Peter Forward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
